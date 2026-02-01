aa7571a2603b325e801ce1adc764d51cdc77b8f5
Jakub Ratuszniak <jakub.ratusz@gmail.com>
2026-02-01 16:39:01 +0100

    refactor(simplifier): use graph for patch generation

--- backend/internal/simplifier/simplifier.go
+++ backend/internal/simplifier/simplifier.go
@@ -3,16 +3,18 @@ package simplifier
 import (
 	"encoding/json"
 	"fmt"
-	"reflect"
 	"slices"
 
+	"github.com/Kuba0517/iam-analyzer/internal/graph"
 	"github.com/Kuba0517/iam-analyzer/internal/model"
 )
 
 func Suggest(p *model.Policy) []model.Patch {
+	g := graph.Build(p)
+
 	var patches []model.Patch
-	patches = append(patches, removeRedundant(p)...)
-	patches = append(patches, mergeStatements(p)...)
+	patches = append(patches, removeRedundant(p, g)...)
+	patches = append(patches, mergeStatements(p, g)...)
 	return patches
 }
 
@@ -32,94 +34,71 @@ func Apply(p *model.Policy, patches []model.Patch, selectedIDs []string) *model.
 	return result
 }
 
-func removeRedundant(p *model.Policy) []model.Patch {
+func removeRedundant(p *model.Policy, g *graph.Graph) []model.Patch {
 	var patches []model.Patch
-	counter := 0
 
-	for i := 0; i < len(p.Statement); i++ {
-		for j := i + 1; j < len(p.Statement); j++ {
-			if reflect.DeepEqual(p.Statement[i], p.Statement[j]) {
-				removeIdx := j
-				id := fmt.Sprintf("dedup-%d", counter)
-				counter++
+	for i, e := range g.EdgesOfType(graph.Redundant) {
+		removeIdx := e.To
+		id := fmt.Sprintf("dedup-%d", i)
 
-				patches = append(patches, model.Patch{
-					ID:          id,
-					Title:       fmt.Sprintf("Remove redundant statement %d", removeIdx),
-					Impact:      "Removes 1 duplicate statement",
-					DiffPreview: removeDiffPreview(p, removeIdx),
-					Apply: func(policy *model.Policy) *model.Policy {
-						cp := deepCopyPolicy(policy)
-						cp.Statement = slices.Delete(cp.Statement, removeIdx, removeIdx+1)
-						return cp
-					},
-				})
-			}
-		}
+		patches = append(patches, model.Patch{
+			ID:          id,
+			Title:       fmt.Sprintf("Remove redundant statement %d", removeIdx),
+			Impact:      "Removes 1 duplicate statement",
+			DiffPreview: removeDiffPreview(p, removeIdx),
+			Apply: func(policy *model.Policy) *model.Policy {
+				cp := deepCopyPolicy(policy)
+				cp.Statement = slices.Delete(cp.Statement, removeIdx, removeIdx+1)
+				return cp
+			},
+		})
 	}
 
 	return patches
 }
 
-func mergeStatements(p *model.Policy) []model.Patch {
+func mergeStatements(p *model.Policy, g *graph.Graph) []model.Patch {
 	var patches []model.Patch
 	counter := 0
 
-	for i := 0; i < len(p.Statement); i++ {
-		for j := i + 1; j < len(p.Statement); j++ {
-			a := p.Statement[i]
-			b := p.Statement[j]
+	for _, e := range g.EdgesOfType(graph.MergeableAction) {
+		mergeI, mergeJ := e.From, e.To
+		id := fmt.Sprintf("merge-%d", counter)
+		counter++
 
-			if a.Effect != b.Effect {
-				continue
-			}
-			if !reflect.DeepEqual(a.Condition, b.Condition) {
-				continue
-			}
-			if !reflect.DeepEqual(a.Principal, b.Principal) {
-				continue
-			}
+		patches = append(patches, model.Patch{
+			ID:          id,
+			Title:       fmt.Sprintf("Merge actions of statements %d and %d", mergeI, mergeJ),
+			Impact:      "Combines 2 statements into 1 by merging Actions",
+			DiffPreview: mergeDiffPreview(p, mergeI, mergeJ, "actions"),
+			Apply: func(policy *model.Policy) *model.Policy {
+				cp := deepCopyPolicy(policy)
+				merged := unionStrings(cp.Statement[mergeI].Action, cp.Statement[mergeJ].Action)
+				cp.Statement[mergeI].Action = merged
+				cp.Statement = slices.Delete(cp.Statement, mergeJ, mergeJ+1)
+				return cp
+			},
+		})
+	}
 
-			if reflect.DeepEqual(a.Resource, b.Resource) && !reflect.DeepEqual(a.Action, b.Action) {
-				mergeI, mergeJ := i, j
-				id := fmt.Sprintf("merge-%d", counter)
-				counter++
+	for _, e := range g.EdgesOfType(graph.MergeableResource) {
+		mergeI, mergeJ := e.From, e.To
+		id := fmt.Sprintf("merge-%d", counter)
+		counter++
 
-				patches = append(patches, model.Patch{
-					ID:          id,
-					Title:       fmt.Sprintf("Merge actions of statements %d and %d", mergeI, mergeJ),
-					Impact:      "Combines 2 statements into 1 by merging Actions",
-					DiffPreview: mergeDiffPreview(p, mergeI, mergeJ, "actions"),
-					Apply: func(policy *model.Policy) *model.Policy {
-						cp := deepCopyPolicy(policy)
-						merged := unionStrings(cp.Statement[mergeI].Action, cp.Statement[mergeJ].Action)
-						cp.Statement[mergeI].Action = merged
-						cp.Statement = slices.Delete(cp.Statement, mergeJ, mergeJ+1)
-						return cp
-					},
-				})
-			}
-
-			if reflect.DeepEqual(a.Action, b.Action) && !reflect.DeepEqual(a.Resource, b.Resource) {
-				mergeI, mergeJ := i, j
-				id := fmt.Sprintf("merge-%d", counter)
-				counter++
-
-				patches = append(patches, model.Patch{
-					ID:          id,
-					Title:       fmt.Sprintf("Merge resources of statements %d and %d", mergeI, mergeJ),
-					Impact:      "Combines 2 statements into 1 by merging Resources",
-					DiffPreview: mergeDiffPreview(p, mergeI, mergeJ, "resources"),
-					Apply: func(policy *model.Policy) *model.Policy {
-						cp := deepCopyPolicy(policy)
-						merged := unionStrings(cp.Statement[mergeI].Resource, cp.Statement[mergeJ].Resource)
-						cp.Statement[mergeI].Resource = merged
-						cp.Statement = slices.Delete(cp.Statement, mergeJ, mergeJ+1)
-						return cp
-					},
-				})
-			}
-		}
+		patches = append(patches, model.Patch{
+			ID:          id,
+			Title:       fmt.Sprintf("Merge resources of statements %d and %d", mergeI, mergeJ),
+			Impact:      "Combines 2 statements into 1 by merging Resources",
+			DiffPreview: mergeDiffPreview(p, mergeI, mergeJ, "resources"),
+			Apply: func(policy *model.Policy) *model.Policy {
+				cp := deepCopyPolicy(policy)
+				merged := unionStrings(cp.Statement[mergeI].Resource, cp.Statement[mergeJ].Resource)
+				cp.Statement[mergeI].Resource = merged
+				cp.Statement = slices.Delete(cp.Statement, mergeJ, mergeJ+1)
+				return cp
+			},
+		})
 	}
 
 	return patches
